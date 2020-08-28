53dc3a7efae36751b267a3dd97ee061b12486d95
Sarah Chun <66026667+sarahschun@users.noreply.github.com>
2020-08-28 10:59:18 -0400

    2nd commit

---
+++ tuples.py
@@ -0,0 +1,8 @@
+# tuples are lists where you can store various info
+# i.e. coordinates
+# inmmutable (cannot be changed/modified)
+#  lists use [], tuples use ()
+
+coordinates = (4, 5)
+# prints out '4'
+print(coordinates[0])
